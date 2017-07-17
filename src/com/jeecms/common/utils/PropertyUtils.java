2a8d3160e9bc947a82e9bd7e2c323d63ec4f98f9
QueryNZ <QueryNZ@192.168.1.101>
2017-07-18 07:00:45 +0800

    完善二级目录-01

---
+++ src/com/jeecms/common/utils/PropertyUtils.java
@@ -0,0 +1,25 @@
+package com.jeecms.common.utils;
+
+import java.util.List;
+
+/**
+ * 资源加载工具
+ * 
+ * @author QueryNZ
+ * @date 2017年7月15日 下午1:12:04
+ */
+public class PropertyUtils {
+
+	
+	/**
+	 * 
+	 * 2017年7月15日 下午3:08:25
+	 * ps: 此方法无非是为了获取hibernate 的xml 文件列表。那么如何用驱动的方式进行测试？
+	 * 本质上是为了读取映射关系。
+	 * @param
+	 */
+	public List<String> getList(String prefix){
+		return null;
+		
+	}
+}
---
+++ src/com/jeecms/common/utils/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 2017年7月15日 下午1:10:24
+ * ps: 工具包
+ * @param 
+ */
+/**
+ * @author QueryNZ
+ * @date 2017年7月15日 下午1:10:24
+ */
+package com.jeecms.common.utils;
